a51a53b2701919f8ae81b633f0f5c0df4a88ead8
Alex Ford <fordas@uw.edu>
2013-12-04 21:41:47 -0800

    Update pose_display to pass repr entries to underlying PDBEmbed.

--- ipython_glmol/display_hooks.py
+++ ipython_glmol/display_hooks.py
@@ -11,7 +11,26 @@ def pdb_display(pdb_id):
 
     return PDBEmbed("\n".join(pdb_lines), "")
 
-def pose_display(pose):
+def extract_character_spans(string):
+    """Extract all contiguous character spans from given string.
+    
+    returns - Character, span structured array:
+            array(num_spans, [("type", "start", "end")])
+    """
+    import numpy
+
+    if isinstance(string, basestring):
+        string = numpy.fromstring(string, "S1")
+    elif isinstance(string, numpy.array) and string.dtype == numpy.dtype("S1"):
+        pass
+    else:
+        raise ValueError("Unable to process input string: %r" % string)
+
+    char_edges = list(numpy.flatnonzero(string[1:] != string[:-1]) + 1)
+
+    return numpy.array([(string[start], start, end) for start, end in zip([0] + char_edges, char_edges + [len(string)])], dtype=[("type", "S1"), ("start", int), ("end", int)])
+
+def pose_display(pose, **repr_entries):
     """Setup embed display for rosetta.core.pose object with sensible default representation."""
     import re
     import cStringIO as StringIO
@@ -28,15 +47,31 @@ def pose_display(pose):
     if not re.search("[^L]", ss_string):
         ss_string = rosetta.core.scoring.dssp.Dssp(pose).get_dssp_secstruct()
 
-    ss_map = [(ss_string[i-1], pose.pdb_info().number(i)) for i in xrange(1, pose.n_residue() + 1)]
-    helix_res = [r for (s, r) in ss_map if s == "H"]
-    sheet_res = [r for (s, r) in ss_map if s == "E"]
-    
+    ss_spans = extract_character_spans(ss_string)
+
+    sheet_spans = []
+    helix_spans = []
+    for s, start, end in ss_spans:
+        residue_span = "%s-%s" % (pose.pdb_info().number(start), pose.pdb_info().number(end))
+
+        if s == "H":
+            helix_spans.append(residue_span)
+        elif s == "E":
+            sheet_spans.append(residue_span)
+
+    if sheet_spans:
+        repr_entries["sheet"] = "residue %s" % ",".join(r for r in sheet_spans)
+
+    if helix_spans:
+        repr_entries["helix"] = "residue %s" % ",".join(r for r in helix_spans)
+
+
+    if not "ribbon" in repr_entries:
+        repr_entries["ribbon"] = "all"
+
     return PDBEmbed(
             pdb_string,
-            ribbon= "all",
-            helix = "residue %s" % ",".join(str(r) for r in helix_res),
-            sheet = "residue %s" % ",".join(str(r) for r in sheet_res))
+            **repr_entries)
 
 def setup_display_hooks():
     """Setup display hooks for all currently loaded modules."""
